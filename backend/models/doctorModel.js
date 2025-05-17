fe47631eed55cc18758b48a8263a5d205bbf6da0
M3LL00W <saicharankasarapu2004@gmail.com>
2025-05-17 23:33:27 +0530

    Final frontend

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const doctorSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -9,15 +9,12 @@ const doctorSchema = new mongoose.Schema({
     degree: { type: String, required: true },
     experience: { type: String, required: true },
     about: { type: String, required: true },
-    available: { type: Boolean, required: true },
+    available: { type: Boolean, default: true },
     fees: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} },
     address: { type: Object, required: true },
     date: { type: Number, required: true },
-    slots_booked: { type: Object, default: {} },
+}, { minimize: false })
 
-
-},{minimize:false})
-
-const doctorModel=mongoose.models.doctor || mongoose.model('doctor',doctorSchema)
-
-export default doctorModel
+const doctorModel = mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+export default doctorModel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,16 +3,14 @@ import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
-import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
-        <Header />
-        <SpecialityMenu />
-        <TopDoctors />
-        <Banner />
-        
+      <Header />
+      <SpecialityMenu />
+      <TopDoctors />
+      <Banner />
     </div>
   )
 }
